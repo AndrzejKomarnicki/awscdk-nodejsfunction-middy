c5c6393bbfad70bda2b7e8574f8b338da6d2975a
Andrzej Komarnicki <andrzej_komarnicki@hotmail.com>
2022-12-04 12:11:47 -0500

    added httpRouterHandler middleware, updated deps

--- services/node-lambda/index.ts
+++ services/node-lambda/index.ts
@@ -2,6 +2,7 @@ import middy from '@middy/core'
 import jsonBodyParser from '@middy/http-json-body-parser'
 import httpErrorHandler from '@middy/http-error-handler'
 import httpSecurityHeaders from '@middy/http-security-headers'
+import httpRouterHandler from '@middy/http-router'
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda'
 import { Logger, injectLambdaContext } from '@aws-lambda-powertools/logger'
 
@@ -10,7 +11,7 @@ const logger = new Logger({
   serviceName: 'middy-example-api',
 });
 
-async function lambdaHandler(event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> {
+async function lambdaHandler(event: APIGatewayProxyEventV2, context: any): Promise<APIGatewayProxyResultV2> {
   // the returned response will be checked against the type `APIGatewayProxyResultV2`
   logger.info('This is a INFO log with some context');
   console.log('event 👉', event);
@@ -20,8 +21,24 @@ async function lambdaHandler(event: APIGatewayProxyEventV2): Promise<APIGatewayP
   }
 }
 
+// routes served by httpRouterHandler middleware, you can add more handlers for routes as needed
+const routes = [
+  {
+    method: 'GET',
+    path: '/user/{id}',
+    handler: lambdaHandler
+  },
+  {
+    method: 'GET',
+    path: '/post/{id}',
+    handler: lambdaHandler
+  }
+]
+
+
 export const handler = middy(lambdaHandler)
   .use(jsonBodyParser())
   .use(httpSecurityHeaders())
   .use(httpErrorHandler())
-  .use(injectLambdaContext(logger)); // Change to (logger, { logEvent: true }) to log the incoming event
+  .use(injectLambdaContext(logger)) // Change to (logger, { logEvent: true }) to log the incoming event
+  .handler(httpRouterHandler(routes))
